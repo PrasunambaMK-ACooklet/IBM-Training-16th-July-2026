a903a868c1fa7ca86be2f1c91eca84b01e1923e3
Prasunamba.MK <Prasunamba.mk@gmail.com>
2026-07-27 18:38:40 +0530

    Demos for ref

---
+++ Demo001_comments.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Demo001_comments {
+    int default_var = 10;
+    public void color() {
+        System.out.println("colors" + default_var);
+    }
+    public static void main(String[] args) {
+        // TODO: code here
+        System.out.println("comments are of 2 types ");
+        System.out.println("single line comments denoted // ");
+        System.out.println("multiple line comments demoted /* write something */");
+    }
+}
